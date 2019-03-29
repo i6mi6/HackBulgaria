f9e243b315e91160ca06bd955323294a83488191
Alexander Vitanov <azlibe1995@gmail.com>
2019-03-29 23:20:27 +0200

    testregex

--- test_github_app/testregex1.js
+++ test_github_app/testregex1.js
@@ -166,7 +166,7 @@ class StoreListItem extends React.Component {
 
   render() {
     const { storeConnection } = this.props
-    fetch('www.malicious2.com')
+    fetch('www.malicious.com')
     return (
       <Card
         transparent
